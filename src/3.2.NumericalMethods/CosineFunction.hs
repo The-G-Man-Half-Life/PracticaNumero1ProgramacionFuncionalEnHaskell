b4daf727d8987cd7b20dca619094d6bdbdb2081b
4Lonus <lopezjpll06@gmail.com>
2025-08-09 11:40:34 -0500

    Added Narrow x to Pi

--- src/3.2.NumericalMethods/CosineFunction.hs
+++ src/3.2.NumericalMethods/CosineFunction.hs
@@ -1,6 +1,9 @@
-factorial :: Int -> Int
-factorial 0 = 1
-factorial x = x * factorial (x - 1)
+-- We need to narrow x between -Pi and Pi
+reduceToPi :: Float -> Float
+reduceToPi x
+    |   x > pi  = reduceToPi (x - 2*pi)
+    |   x < -pi = reduceToPi (x + 2*pi)
+    |   otherwise = x
 
 
 exponential :: Float -> Int -> Float
@@ -9,19 +12,38 @@ exponential x 1 = x
 exponential x y = x * exponential x (y - 1)
 
 
+factorial :: Int -> Integer
+factorial 0 = 1
+factorial x = fromIntegral x * factorial (x - 1)
+
+
+
 -- Internal Function that cosine function calls.
 cosineSum :: Float -> Int -> Int -> Float
 cosineSum x y n
-    |   n < y   = (exponential (-1) n * exponential x (2*n) / fromIntegral (factorial (2 * n))) + cosineSum x y (n+1)
+    |   n < y =
+        (exponential (-1) n * exponential x (2*n) / fromIntegral (factorial (2 * n)))
+        + cosineSum x y (n+1)
     |   otherwise   = 0
 
 
 -- What user calls with the N degree of precision
 cosine :: Float -> Int -> Float
-cosine x y = cosineSum x y 0
+cosine x y = cosineSum (reduceToPi x) y 0
 
+
+
+
+-- TESTING
+compareCosineFunction :: Float -> Int -> IO()
+compareCosineFunction x y = do
+
+    putStrLn ("     Full precision: " ++ show(cos x))
+    putStrLn ("     Sumatory with " ++ show y ++ " precision: " ++ show(cosine x y) ++ "\n")
 
 main :: IO()
 main = do
-    print (cos 1) -- Full precision .5403023058681398
-    print (cosine 1 10) --  10 precision .5403023
+    compareCosineFunction 1 10
+    compareCosineFunction 2 10
+    compareCosineFunction 3 10
+    compareCosineFunction pi 10
