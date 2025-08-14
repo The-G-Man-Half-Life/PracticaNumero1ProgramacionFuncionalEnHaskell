946c25f7bc82828d80bea15c08b7b2de3cc5d2ad
4Lonus <lopezjpll06@gmail.com>
2025-08-13 23:23:02 -0500

    Update naturalLogarithm.hs

--- src/3_2_NumericalMethods/naturalLogarithm.hs
+++ src/3_2_NumericalMethods/naturalLogarithm.hs
@@ -1,37 +1,53 @@
-absoluteValue :: Double -> Double
-absoluteValue x
+absValue :: Float -> Float
+absValue x
     |   x >= 0      = x
     |   otherwise   = -x
 
 
-exponential :: Double -> Int -> Double
+exponential :: Float -> Int -> Float
 exponential _ 0 = 1
 exponential x 1 = x
 exponential x y = x * exponential x (y-1)
 
 
 -- Internal function Natural Logarithm function calls
-naturalLogSum :: Double -> Int -> Int -> Double
+naturalLogSum :: Float -> Int -> Int -> Float
 naturalLogSum x y n
-    |   n < y       = (exponential (-1) (n+1) * exponential x n / fromIntegral n) + naturalLogSum x y (n+1)
+    |   n <= y       = (exponential (-1) (n+1) * (exponential x n / fromIntegral n)) + naturalLogSum x y (n+1)
     |   otherwise   = 0
 
 
 -- Main function user calls with N degree of precision
-naturalLog :: Double -> Int -> Double
+naturalLog :: Float -> Int -> Float
 naturalLog x n
-    |   absoluteValue x > 1 = naturalLogSum x n 0
-    
+    |   absValue x < 1 = naturalLogSum x n 1
+    |   otherwise = 0
 
---  TESTING
-compareLogarithmFunction :: Double -> Int -> IO()
-compareLogarithmFunction x y = do
-    putStrLn ("     Full precision: " ++ show(log x))
-    putStrLn ("     Sumatory with " ++ show y ++ " precision: " ++ show(naturalLog x y) ++ "\n")
 
+
+
+
+--  JUST TESTING
+rounded :: Float -> Float -> Float
+rounded x dy =
+    let factor = 10.0 **  fromIntegral(round (logBase 10 dy))
+    in fromIntegral (round (x * factor)) / factor
+
+compareFunction :: Float -> Int -> IO()
+compareFunction x y = do
+    putStrLn (show x ++ "\t\tFull precision:\t\t\t" ++ show (log (1+x)))
+    putStrLn ("\t\tSumatory (" ++ show y ++ " precision):\t" ++ show (naturalLog x y) ++ "\n")
+
+testInRange :: Float -> Float -> Float -> Int -> IO()
+testInRange x dy z a = do
+    if x <= z then do
+        let rx = rounded x dy
+        compareFunction rx a
+        testInRange (rx + dy) dy z a
+    else print "End of Test"
 
 main :: IO ()
 main = do
-    compareLogarithmFunction (-1) 10
-    compareLogarithmFunction 2 10
-    compareLogarithmFunction 10 15
+    testInRange (-1.02) 0.01 1.02 200
+
+--  FINISH TESTING
