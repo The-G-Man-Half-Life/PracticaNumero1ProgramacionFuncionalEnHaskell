5d25963472ba0e1983569ae372ae704e71da3c73
4Lonus <lopezjpll06@gmail.com>
2025-08-23 20:39:56 -0500

    Update sortArray.hs
    Why did you changed the folder names?

--- src/P3_1_UsefulFunctions/sortArray.hs
+++ src/P3_1_UsefulFunctions/sortArray.hs
@@ -1,28 +1,68 @@
--- Checks if the Array it's in a decending order.
-isDecending :: [Float] -> Bool
-isDecending [] = True
-isDecending [x] = True
-isDecending (x:y:xs) 
+{-
+Checks whatever a given array is on a descending order.
+
+Input: [Float]
+	Finite array of Float.
+
+Output:	Boolean
+	-> True:	If there is only 0 or 1 elements on the array.
+	-> False:	If detected a single pair of elements out of order.
+
+Notes:
+	- Array must be finite, otherwise function may not terminate.
+	- In order to compute the order of any array we must rely on recursion and just analyze a single pair at a time:
+        isdescending [n1, n2, n3, ..., nx] <=> (n1 >= n2) & isdescending [n2, n3, ..., nx]
+-}
+isdescending :: [Float] -> Bool
+isdescending [] = True
+isdescending [x] = True
+isdescending (x:y:xs) 
     |   x < y       = False
-    |   otherwise   = isDecending(y:xs)
+    |   otherwise   = isdescending(y:xs)
 
 
--- Switches elements a single time.
+
+{-
+Makes a single round of swapping every pair of elements out of order.
+
+Input:	[Float]
+	Finite array of Float.
+
+Output:
+	Array after a single iteration of swapping out of order elements.
+
+Notes:
+	- If array is already ordered then just returns array.
+-}
 swapElementDes :: [Float] -> [Float]
 swapElementDes [x] = [x]
 swapElementDes (x:y:xs)
-    |   isDecending (x:y:xs) = x:y:xs -- If sorted Array, just returns
-    |   x < y       = y : swapElementDes (x:xs)
+    |   isdescending (x:y:xs) = x:y:xs -- If sorted Array, just returns
+    |   x < y       = y : swapElementDes (x:xs) -- Swaps if element is not descending.
     |   otherwise   = x : swapElementDes (y:xs)
 
 
--- Checks if the array is sorted, if it's not swaps elements once and checks again.
+
+{-
+Loops checking if array is in a descending order.
+If it's not, swaps every out of order pair of elemets and checks again.
+
+Input:	[Float]
+	Finite array of Float.
+
+Output:	[Float]
+	Array of Float in a descending order.
+-}
 orderDes :: [Float] -> [Float]
 orderDes xs
-    |   isDecending xs  = xs
+    |   isdescending xs  = xs
     |   otherwise       = orderDes(swapElementDes xs)
 
---  TESTING --
+
+
+
+
+--	TESTING
 main :: IO()
 main = do
     let array = [1, 25, 5, -4, 88, -1.4, -95, 0.0, 9.8]
